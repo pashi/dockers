5033c02c2dc3ac36c15ff7777abf5e4215e93c2e
Pasi Lammi <pasi.lammi@enfogroup.com>
2021-03-30 12:40:02 +0300

    add tags and csv

--- feedreader/feedreader.py
+++ feedreader/feedreader.py
@@ -1,9 +1,10 @@
 import feedparser
-import json
-import os, time, glob, logging, sys
+import json, csv
+import os, time, glob, logging, sys, datetime
 from datetime import date
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+import azure.core
 
 # xx
 # * FEED_URL
@@ -80,6 +81,27 @@ def write_json(fname,data, indent=0):
     except:
         pass
 
+def write_csv(fname,data):
+    logger.debug('write_csv:%s' % (fname))
+    #try:
+    with open(fname, 'w') as f:
+            writer = csv.writer(f, delimiter=';',quotechar='|',quoting=csv.QUOTE_MINIMAL)
+            fieldnames = ['ts', 'id', 'link', 'title', 'summary','tags']
+            writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=';',quotechar='"', quoting=csv.QUOTE_ALL)
+            writer.writeheader()
+            for item in data:
+                dt = datetime.datetime.utcfromtimestamp(item['ts'])
+                iso_format = dt.isoformat() + 'Z'
+                item['ts'] = iso_format
+                del item['published_parsed']
+                if len(item['tags']) > 0:
+                    item['tags'] = ','.join(item['tags'])
+                else:
+                    item['tags'] = ''
+                writer.writerow(item)
+    #except:
+    #    pass
+
 def get_blob_client(fname):
     blob_service_client = BlobServiceClient.from_connection_string(azure_blob)
     blob_client = blob_service_client.get_blob_client(container=azure_blob_container, blob=fname)
@@ -87,14 +109,24 @@ def get_blob_client(fname):
 
 def upload_blob(fname):
     blob_client = get_blob_client(fname)
+    content_type = None
+    x,y = os.path.splitext(fname)
+    if y == '.json':
+        content_type = 'application/json'
+    elif y == '.csv':
+        content_type = 'text/csv'
     with open(fname, "rb") as data:
-        blob_client.upload_blob(data, blob_type="BlockBlob", overwrite=True)
+        blob_client.upload_blob(data, blob_type="BlockBlob", overwrite=True, content_type=content_type)
 
 def download_blob(fname):
-    blob_client = get_blob_client(fname)
-    with open(fname, "wb") as data:
-        download_stream = blob_client.download_blob()
-        data.write(download_stream.readall())
+    try:
+        blob_client = get_blob_client(fname)
+        with open(fname, "wb") as data:
+            download_stream = blob_client.download_blob()
+            data.write(download_stream.readall())
+    except azure.core.exceptions.ResourceNotFoundError:
+        # no blob found.. continue
+        logger.warning('download_blob:not_found:%s' % (fname))
 
 def read_json_data(fname):
     logger.debug('read_json_data:%s' % (fname))
@@ -127,7 +159,7 @@ def read_rss(url):
         ## edit here if you would like to change some fields what to store
         xx = ['id', 'link','title','summary','published_parsed']
         for entry in feed['entries']:
-            d = {}
+            d = {'tags':[]}
             for x in xx:
                 d[x] = getattr(entry, x, None)
             d_tp = time.strftime("%Y%m", d['published_parsed'])
@@ -139,6 +171,17 @@ def read_rss(url):
 
             if not d_tp in tp:
                 tp[d_tp] = []
+            if 'tags' in entry:
+                tag_list = {}
+                for tag in entry['tags']:
+                    if 'term' in tag:
+                        t_tag_name = tag['term']
+                        for t_k in t_tag_name.split(';'):
+                            t_k_fixed = t_k.strip().lower()
+                            if not t_k_fixed == '':
+                                tag_list[t_k_fixed] = True
+                d['tags'] = sorted(list(tag_list.keys()))
+
             tp[d_tp].append(d)
     del feed
     return tp
@@ -181,18 +224,24 @@ rss_data = read_rss(url)
 write_json(rssfilename, rss_data, 2)
 
 d = {}
+files_to_upload = [ rssfilename, feedfilename ]
 for time_partition in sorted(rss_data.keys(), reverse=False):
     old_rss = get_old_tp_data(time_partition)
     new_rss = rss_data[time_partition]
     data = get_distinct(old_rss,new_rss)
     fname = 'data.%s.json' % (time_partition)
+    cfname = 'data.%s.csv' % (time_partition)
     write_json(fname, data, 2)
+    write_csv(cfname,data)
+    files_to_upload.append(fname)
+    files_to_upload.append(cfname)
 
 if use_azure_blob:
     x = glob.glob('data.*.json')
-    x.append(rssfilename)
-    x.append(feedfilename)
     for f in x:
+        files_to_upload.append(f)
+
+    for f in files_to_upload:
         logger.debug('upload:%s' % (f))
         upload_blob(f)
 
